f85aee4e2938773c9aebcc6f7cb57ebfa58b5da4
eraycancelik <cancelikeray@gmail.com>
2023-09-07 15:01:37 +0300

    cartlist bug still there

--- yedek/src/Components/Card/PersonIcon.tsx
+++
@@ -1,9 +0,0 @@
-type PersonIcon = {
-  className: string;
-  src:string;
-};
-const PersonIcon = (props: PersonIcon) => {
-  return <img src={props.src} className={props.className} />;
-};
-
-export default PersonIcon;
--- yedek/src/Data/categories.tsx
+++
@@ -1,7 +0,0 @@
-export const categories: string[] = [
-  "electronic",
-  "motor",
-  "component",
-  "mechanic",
-  "measuring instruments",
-];
