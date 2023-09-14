f746bd2d00f7bf444a0dffea3680b7021067ac89
eraycancelik <cancelikeray@gmail.com>
2023-09-14 20:17:52 +0300

    passed to route

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,19 @@
-import React, { useEffect, useState } from "react";
-import Header from "./Components/Layout/Header";
-import style from "./App.module.css";
-import ProductList from "./Components/Product/ProductList";
-import SideBar from "./Components/SideBar/SideBar";
-import Information from "./Components/Ui/Information";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Components/Layout/Footer";
-import { products } from "./Data/products";
-import { useToggleStore } from "./states/clearState";
-import { categories } from "./Data/categories";
-interface Product {
-  Product_id: number;
-  Product: string;
-  Photo: string;
-  Price: number;
-  Category: string;
-}
-type Props = {};
-
-const App: React.FC<Props> = () => {
-  const { closeToggle, openToggle } = useToggleStore((state: any) => ({
-    closeToggle: state.closeToggle,
-    openToggle: state.openToggle,
-  }));
-  const [categoryHandlers, setCategoryHandlers] = useState<string[]>([]);
-  const [productsArea, setProductsArea] = useState<Product[]>(products);
-  const [pricesArea, setPricesArea] = useState<number[]>(
-    products.map((product) => product.Price)
-  );
-  let filteredProducts: Product[] = [];
-  let prices: number[] = [];
-
-  const categoryHandler = (category: string[]) => {
-    setCategoryHandlers(category);
-  };
-  const [sliderPrices, setSliderPrices] = useState<number[]>([
-    Math.min(1),
-    Math.max(300),
-  ]);
-  const sliderPricesHolder = (values: number[]) => {
-    setSliderPrices(values);
-  };
-
-  useEffect(() => {
-    if (categoryHandlers.length === 0) {
-      const newProducts: Product[] = products.filter((product) => {
-        return (
-          product.Price >= sliderPrices[0] && product.Price <= sliderPrices[1]
-        );
-      });
-      setProductsArea(newProducts);
-      setPricesArea(products.map((product) => product.Price));
-    } else {
-      const filteredProducts: Product[] = [];
-      const prices: number[] = [];
-
-      categoryHandlers.forEach((category) => {
-        const categoryProducts: Product[] = products.filter(
-          (product) =>
-            product.Category === category &&
-            product.Price >= sliderPrices[0] &&
-            product.Price <= sliderPrices[1]
-        );
-        filteredProducts.push(...categoryProducts);
-        prices.push(...categoryProducts.map((product) => product.Price));
-      });
-      setPricesArea(prices);
-      setProductsArea(filteredProducts);
-    }
-  }, [categoryHandlers, sliderPrices]);
-
-  const clearAllFilters = () => {
-    setPricesArea([1, 300]);
-    setSliderPrices([1, 300]);
-    setProductsArea(products);
-    closeToggle();
-  };
-
+import Header from "./Components/Layout/Header";
+import ProductsPage from "./Pages/ProductsPage";
+import ProfilePage from "./Pages/ProfilePage";
+import "./App.css";
+const App = () => {
   return (
-    <div className={style.app}>
+    <BrowserRouter>
       <Header />
-      <div className={style.flexContainer}>
-        <div className={style.flex}>
-          <Information />
-        </div>
-        <div className={style.gridContainer}>
-          <SideBar
-            clearAllFilters={clearAllFilters}
-            onPriceHolder={sliderPricesHolder}
-            pricesList={pricesArea}
-            categoryHandler={categoryHandler}
-          />
-          <ProductList productList={productsArea} />
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<ProductsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 };
 
---
+++ src/Pages/ProfilePage.tsx
@@ -0,0 +1,12 @@
+import style from "./ProfilePage.module.css";
+const ProfilePage = () => {
+  return (
+    <>
+      <div className={style.main}>
+        <h1>Profile</h1>
+      </div>
+    </>
+  );
+};
+
+export default ProfilePage;
